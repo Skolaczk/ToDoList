f9b5301bacd9692a25df2ca12b19246710c84225
Skolaczk <skolakmichal1@gmail.com>
2021-10-09 18:29:25 +0200

    Add initial files

---
+++ src/assets/styles/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html{
+    box-sizing: border-box;
+  }
+
+  *, *::before, *::after{
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    background-color: ${({ theme }) => theme.colors.lightGray};
+}
+`;
---
+++ src/assets/styles/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    lightGray: '#E5E5E5',
+    white: '#FFFFFF',
+    gray: '#737C8E',
+  },
+  fontWeight: {
+    regular: '400',
+    bold: '700',
+  },
+  fontSize: {
+    m: '17px',
+  },
+};
